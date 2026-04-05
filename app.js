86a57a415985d853cf509b5ddc1b103e1ab534d6
Ashrut Yadav <ashrutyadav7@gmail.com>
2026-04-05 21:44:46 +0530

    Add root route to confirm server is running

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(mockUser);
-
+app.get('/', (req, res) => {
+  res.send("Finance Backend is running 🚀");
+});
 app.use('/users', userRoutes);
 app.use('/records', recordRoutes);
 app.use('/summary', summaryRoutes);
