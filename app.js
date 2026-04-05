c5f2ddea3d1910bed334fe28872de34db6f94b14
Ashrut Yadav <ashrutyadav7@gmail.com>
2026-04-05 21:45:29 +0530

    added route to ensure backend is running

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(mockUser);
 app.get('/', (req, res) => {
-  res.send("Finance Backend is running 🚀");
+  res.send("Finance Backend is running");
 });
 app.use('/users', userRoutes);
 app.use('/records', recordRoutes);
